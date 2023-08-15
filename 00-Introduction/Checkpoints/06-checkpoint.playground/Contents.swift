3c69276cc2f4466ceff77067b800bed1b542314c
Zain Karim <zain@zainkarim.com>
2023-08-14 20:14:35 -0500

    Completed Challenge 6

---
+++ 00-Introduction/Checkpoints/06-checkpoint.playground/Contents.swift
@@ -0,0 +1,52 @@
+import Cocoa
+
+/* Create a struct to store information about a car. Include:
+    - Its model
+    - Number of seats
+    - Current gear
+ * Add a method (or methods) to change gears up or down
+ * Have a think about variables and access control.
+ * Don't allow invalid gears - 1...10 seems like a fair maximum range.
+ HINTS:
+ * A car’s model and seat count aren’t going to change once produced, so they can be constant. But its current gear clearly does change, so make that a variable.
+ * Changing gears up or down should ensure such a change is possible – there is no gear 0, for example, and it’s safe to assume a maximum of 10 gears should cover most if not all cars.
+ * If you use private access control, you will probably also need to create your own initializer. (Is private the best choice here? Try it for yourself and see what you think!)
+ * Remember to use the mutating keyword on methods that change properties! */
+
+struct Car {
+    let model: String
+    let numberOfSeats: Int
+    private(set) var currentGear = 1 // Let's make first gear the default value.
+    
+    mutating func upShift(shifts: Int) -> Bool {
+        if currentGear >= 1 && currentGear + shifts < 10 {
+            currentGear += shifts
+            print("Upshifting to gear number \(currentGear).")
+            return true
+        } else {
+            print("Can't shift that high!")
+            return false
+        }
+    }
+    
+    mutating func downShift(shifts: Int) -> Bool {
+        if currentGear <= 10 && currentGear - shifts >= 1 {
+            currentGear -= shifts
+            print("Downshifting to gear number \(currentGear).")
+            return true
+        } else {
+            print("Can't shift that low!")
+            return false
+        }
+    }
+}
+
+var accord = Car(model: "Honda Accord", numberOfSeats: 5)
+print("Model: \(accord.model)")
+print("Number of seats: \(accord.numberOfSeats)")
+print("Current gear: \(accord.currentGear)")
+
+accord.upShift(shifts: 6)
+accord.downShift(shifts: 5)
+accord.downShift(shifts: 1)
+accord.downShift(shifts: 1)
---
+++ 00-Introduction/Lessons/11-Structs Part 2.playground/Contents.swift
@@ -0,0 +1,109 @@
+import Cocoa
+
+// 1. ===== Limiting access to internal data using access control =====
+
+struct BankAccount {
+    var funds = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds > amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount:100)
+
+let success = account.withdraw(amount:200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Insufficient funds")
+}
+
+// In our struct, the funds property is exposed to us externally. There's nothing stopping us from touching it directly:
+account.funds -= 1000 // Here we're withdrawing much more money than we have initially deposited. It completely bypasses the logic we had that verifies that the user has the funds before withdrawing anything.
+
+// To solve this, we can tell Swift that the funds property should only be available to be read and written to only inside the struct by methods that belong to it (deposit and withdraw).
+
+struct BankAccountV2 {
+    private(set) var funds = 0 // Now funds cannot be read from outside of the struct. This is called access control.
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds > amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+/* Swift gives us several options when it comes to access control:
+ 1. private: Don't let anything outside the struct use this.
+ 2. filePrivate: Don't let anything outside of the current file use this.
+ 3. public: Anyone anywhere can use this
+ 4. private(set): Anyone can read the property, but it can only be written/modified by internal methods. */
+// Access control is about limiting what you and other devs can do.
+// Important tip ⭐️ : if you have a private access control property in your struct, there's a good chance you have to create your own initializer.
+
+// 2. =============== Static properties and methods ====================
+
+/* Sometimes, you want to add a property or method to a struct itself, not to a single instance of it. This lets you access it directly. This is used in SwiftUI for 2 reasons.
+ 1. Example data
+ 2. Constant data that needs to be accessed in various places in your program. */
+
+struct School {
+    static var studentCount = 0
+    // Notice the static keyword here: this means both studentCount and the add method belong to the School struct itself, not to an individual instance of a School.
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+// The property and method can be accessed directly on the struct. This is because they're both static. They don't exist uniquely in every school instance, but exist only once across all school instances. They're all shared! This is why we can say studentCount += 1 without marking the add method as mutating.
+// ⭐️ Remember: we only have to have 'mutating' as a keyword when the struct might be a constant or a variable. In this instance, it's always a struct itself.
+School.add(student: "James Bond")
+print(School.studentCount)
+
+/* If you want to mix and match static and non-static properties and methods, there are two rules:
+ 1. If you want to access non-static stuff from static stuff, YOU CANNOT!
+ 2. If you're in a non-static method/property, you can read the static versions. ----> School.studentCount
+                        or
+                     Self.studentCount
+                    ⭐️ Remember:
+                        - lowercase s self means the current value of a struct.
+                        - Capital S struct means the current type of struct.
+ Why is this needed? */
+// 1. We can use static properties to organize common data in a project:
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+// Using this approach, every time we need to ccheck some information about the app's version number (about screen, debug output, logging info, etc) we can just read AppData.version anywhere in the program.
+
+// 2. We can use static data to store example data for structs. SwiftUI works best when you can see previews of your data as you work. These previews often rely on example data.
+struct Employee {
+    let username: String
+    let password: String
+    
+    // You can't use a regular let because that would result in Emplyee containing an employee containing an employee containg ...
+    static let example = Employee(username: "CraigF", password: "hairforceone")
+}
+
+Employee.example
