c1aacf8a7194145904ecfd0875c76eca196b08d7
Zain Karim <zain@zainkarim.com>
2025-07-16 14:10:41 -0500

    Updated checkpoint 2:

--- 00-Introduction/Checkpoints/02-checkpoint.playground/Contents.swift
+++ 00-Introduction/Checkpoints/02-checkpoint.playground/Contents.swift
@@ -1,1 +1,11 @@
+import Cocoa
+
+// Create an array or strings, print number of items, and number of UNIQUE items.
+
+let greetings = (["hello", "goodbye", "hi", "hello", "salutations", "pleased to meet you"])
+let num_items = greetings.count
+let num_unique_items = Set(greetings).count
+
+print("There are \(num_items) items in the greetings array.")
+print("There are \(num_unique_items) unique items in the greetings array.")
 
