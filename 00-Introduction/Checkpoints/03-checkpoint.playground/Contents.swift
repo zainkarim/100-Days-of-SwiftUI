8f0b16e6518d61f3d9a9e887a2fc76b64d1a92cc
Zain Karim <zain@zainkarim.com>
2025-07-16 15:33:10 -0500

    Updated Checkpoint 3

--- 00-Introduction/Checkpoints/03-checkpoint.playground/Contents.swift
+++ 00-Introduction/Checkpoints/03-checkpoint.playground/Contents.swift
@@ -1,14 +1,17 @@
-import Cocoa
-
 // FizzBuzz
+// Loop from 1 to 100
+// If multiple of 3, print "Fizz"
+// If multiple of 5, print "Buzz"
+// If multiple of both, print "FizzBuzz"
+// Otherwise, just print the number.
 
 for i in 1...100 {
-    if i % 5 == 0 && i % 3 == 0 {
+    if i % 3 == 0 && i % 5 == 0 {
         print("FizzBuzz")
-    } else if i % 3 == 0 {
-        print("Fizz")
     } else if i % 5 == 0 {
         print("Buzz")
+    } else if i % 3 == 0 {
+        print("Fizz")
     } else {
         print(i)
     }
